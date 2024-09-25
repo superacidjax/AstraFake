dd9f6214d3974db0d783606b9cfd3d70bb493c4c
Brian Dear <superacidjax@me.com>
2024-09-25 10:44:06 +0200

    reverts to the good old days before we sent type AST-23

--- app/services/data_sender.rb
+++ app/services/data_sender.rb
@@ -16,9 +16,6 @@ class DataSender
 
   def seed_people
     @people_data["people"].each do |person|
-      person["traits"].each do |trait|
-        ensure_trait_has_type(trait)
-      end
       send_post_request("person" => person)
     end
   end
@@ -26,9 +23,6 @@ class DataSender
   def send_random_event
     person = pick_random_person
     event = generate_random_event(person["user_id"])
-    event["properties"].each do |property|
-      ensure_property_has_type(property)
-    end
     send_event_post_request(event)
   end
 
@@ -52,50 +46,51 @@ class DataSender
       {
         "event_type" => "New User Created",
         "user_id" => user_id,
-        "properties" => [
-          { "source" => [ "self-serve", "imported", "employer" ].sample, "type" => "string" }
-        ],
+        "properties" => {
+          "source" => [ "self-serve", "imported", "employer" ].sample
+        },
         "timestamp" => Faker::Time.between(from: 3.years.ago, to: Time.now).iso8601
       }
     when "Screening Scheduled"
       {
         "event_type" => "Screening Scheduled",
         "user_id" => user_id,
-        "properties" => [
-          { "clinic_name" => Faker::Company.name, "type" => "string" },
-          { "date_of_screening" => Faker::Time.between(from: Time.now, to: 1.year.from_now).iso8601, "type" => "datetime" },
-          { "screening_type" => [ "Annual", "Initial", "Specialty" ].sample, "type" => "string" }
-        ],
+        "properties" => {
+          "clinic_name" => Faker::Company.name,
+          "date_of_screening" => Faker::Time.between(from: Time.now, to: 1.year.from_now).iso8601,
+          "screening_type" => [ "Annual", "Initial", "Specialty" ].sample
+        },
         "timestamp" => Faker::Time.between(from: 6.months.ago, to: Time.now).iso8601
       }
     when "Screening Completed"
       {
         "event_type" => "Screening Completed",
         "user_id" => user_id,
-        "properties" => [
-          { "clinic_name" => Faker::Company.name, "type" => "string" },
-          { "date_of_completion" => Faker::Time.between(from: Time.now, to: 1.year.from_now).iso8601, "type" => "datetime" },
-          { "screening_type" => [ "Annual", "Initial", "Specialty" ].sample, "type" => "string" }
-        ],
+        "properties" => {
+          "clinic_name" => Faker::Company.name,
+          "date_of_completion" => Faker::Time.between(from: Time.now, to: 1.year.from_now).iso8601,
+          "screening_type" => [ "Annual", "Initial", "Specialty" ].sample
+        },
         "timestamp" => Faker::Time.between(from: 6.months.ago, to: Time.now).iso8601
       }
     when "New Measurement"
       {
         "event_type" => "New Measurement",
         "user_id" => user_id,
-        "properties" => [
-          { "measurement_type" => "BMI", "bmi" => rand(17.00..41.00).round(2), "type" => "numeric" },
-          { "date_of_measurement" => Faker::Time.between(from: 6.months.ago, to: Time.now).iso8601, "type" => "datetime" }
-        ],
+        "properties" => {
+          "type" => "BMI",
+          "date_of_measurement" => Faker::Time.between(from: 6.months.ago, to: Time.now).iso8601,
+          "bmi" => rand(17.00..41.00).round(2)
+        },
         "timestamp" => Faker::Time.between(from: 6.months.ago, to: Time.now).iso8601
       }
     when "Claim Received"
       {
         "event_type" => "Claim Received",
         "user_id" => user_id,
-        "properties" => [
-          { "date_of_claim" => Faker::Time.between(from: 6.months.ago, to: Time.now).iso8601, "type" => "datetime" }
-        ],
+        "properties" => {
+          "date_of_claim" => Faker::Time.between(from: 6.months.ago, to: Time.now).iso8601
+        },
         "timestamp" => Faker::Time.between(from: 6.months.ago, to: Time.now).iso8601
       }
     when "Lab Received"
@@ -103,27 +98,27 @@ class DataSender
       {
         "event_type" => "Lab Received",
         "user_id" => user_id,
-        "properties" => [
-          { "date_of_lab" => Faker::Time.between(from: 6.months.ago, to: Time.now).iso8601, "type" => "datetime" },
-          { "test_type" => test_type, "type" => "string" },
-          { "result" => generate_lab_result(test_type), "type" => "numeric" }
-        ],
+        "properties" => {
+          "date_of_lab" => Faker::Time.between(from: 6.months.ago, to: Time.now).iso8601,
+          "test_type" => test_type,
+          "result" => generate_lab_result(test_type)
+        },
         "timestamp" => Faker::Time.between(from: 6.months.ago, to: Time.now).iso8601
       }
     when "Form Received"
       {
         "event_type" => "Form Received",
         "user_id" => user_id,
-        "properties" => [
-          { "form_type" => [ "intake", "medical record", "release", "other" ].sample, "type" => "string" }
-        ],
+        "properties" => {
+          "type" => [ "intake", "medical record", "release", "other" ].sample
+        },
         "timestamp" => Faker::Time.between(from: 6.months.ago, to: Time.now).iso8601
       }
     when "Incentive Achieved"
       {
         "event_type" => "Incentive Achieved",
         "user_id" => user_id,
-        "properties" => [],
+        "properties" => {},
         "timestamp" => Faker::Time.between(from: 6.months.ago, to: Time.now).iso8601
       }
     end
@@ -137,31 +132,6 @@ class DataSender
       rand(70..120) # Adjust for Glucose realistic range
     when "A1c"
       rand(4.5..6.5).round(2) # Adjust for A1c realistic range
-    end
-  end
-
-  def ensure_trait_has_type(trait)
-    unless trait.key?("type")
-      trait["type"] = infer_type(trait.values.first)
-    end
-  end
-
-  def ensure_property_has_type(property)
-    unless property.key?("type")
-      property["type"] = infer_type(property.values.first)
-    end
-  end
-
-  def infer_type(value)
-    case value
-    when String
-      "string"
-    when Numeric
-      "numeric"
-    when Time, DateTime
-      "datetime"
-    else
-      "string"
     end
   end
 
