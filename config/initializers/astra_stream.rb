b184e4032fd2767c0f031fa8ea64e43f3fa22ae9
Brian Dear <superacidjax@me.com>
2024-09-30 16:30:41 +0200

    updates data_sender [skip ci]

--- config/initializers/astra_stream.rb
+++ config/initializers/astra_stream.rb
@@ -1,2 +1,3 @@
+# ASTRA_STREAM_URL = ENV.fetch("ASTRA_STREAM_URL", "https://astrastream-f88676dd5abc.herokuapp.com/api/v1")
 ASTRA_STREAM_URL = ENV.fetch("ASTRA_STREAM_URL", "http://localhost:3001/api/v1")
 AUTO_SEND = ENV.fetch("AUTO_SEND", "true")
